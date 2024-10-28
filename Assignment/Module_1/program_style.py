c8e75b3fd146b272abe8ea4d71fad3edba49e3e1
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2024-10-29 02:07:03 +0530

    add file

---
+++ Assignment/Module_1/program_style.py
@@ -0,0 +1,7 @@
+# Write a Python program that demonstrates the correct use of indentation, comments, andvariables following PEP 8 guidelines.
+
+a = 5
+if a > 11:
+    print("a is greater than 10")
+else:
+    print("a is less than 10")
