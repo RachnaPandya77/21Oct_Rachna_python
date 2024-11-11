69e21cc8037b32bff1082e9b193356e56b4316ca
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2024-11-11 21:32:15 +0530

    add new file

---
+++ Assignment/Module1/Fundamentals_of_python/count_occurrences.py
@@ -0,0 +1,10 @@
+#  Write a Python program to count occurrences of a substring in a string
+
+str = "This is testing of substribg"
+substr = "t"
+substr1 = "testing"
+
+print(str.count(substr))
+print(str.count(substr1))
+
+
