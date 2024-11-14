be10435a75aa92af1016dce84b5b7ef37f725e29
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2024-11-14 20:10:28 +0530

    module2

---
+++ Assignment/Module2/1_list.py
@@ -0,0 +1,6 @@
+# Write a Python program to create a list of multiple data type elements
+
+print("Multiple data type")
+mylist = ['Data types', 11, 20.5, "True", ['a','b','c']]
+
+print(mylist)
---
+++ Assignment/Module2/2_insert.py
@@ -0,0 +1,11 @@
+# Write a Python program to add elements to a list using insert() and append().
+
+language = ["Hindi", "English", "Gujarati"]
+language.insert(2, "Marathi")
+print(language)
+
+
+print("-------Append-------")
+language.append("Telegu")
+print(language)
+
