e05b4bb5960f81efb2e1e365296dfcdb174aadd2
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2024-11-20 11:04:15 +0530

    module 2

---
+++ Assignment/Module2/19_1.fun.py
@@ -0,0 +1,51 @@
+# Write a Python program to create a calculator using functions
+
+x = int(input("Enter a number: "))
+y = int(input("Enter a number: "))
+
+print("1. Add")
+print("2. Sub")
+print("3. Mul")
+print("4. Div")
+choice = int(input("Enter a choice: "))
+
+a = x + y
+s = x - y
+m = x * y
+d = x / y
+
+def add():
+    print("add: ",a)
+
+def sub():
+    print("sub: ",s)
+   
+def mul():
+    print("mul: ", m)
+
+def div():
+    print("div: ", d)
+
+if choice == 1 :
+    add()
+
+elif choice == 2:
+    sub()
+
+elif choice == 3:
+    mul()
+
+elif choice == 4:
+    div()
+
+else:
+    print("enter valid choice")
+    
+
+
+
+
+
+
+
+
