06e842a1c46dd8e2798d6a73b0af1cec65cf2018
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2024-12-21 23:29:15 +0530

    add new file

--- Assignment/Module3/8_19overloading.py
+++ Assignment/Module3/8_19overloading.py
@@ -1,2 +1,12 @@
 # Write a Python program to show method overloading
 
+class info:
+    def display_info(self, id):
+        print("Subject ID: ", id)
+
+    def display_info(self,name):
+        print("Sub Name: ",name)
+
+obj = info()
+obj.display_info('Python')
+obj.display_info(123)
