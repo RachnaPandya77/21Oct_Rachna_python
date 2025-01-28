3395015e0809d92c8c14d47bb210faad898b4d9b
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2025-01-29 00:51:48 +0530

    add templates

--- Assignment/Module4/docproject/docproject/settings.py
+++ Assignment/Module4/docproject/docproject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'docproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'docdb',
+        'USER': 'root',
+        'PASSWORD': '',         
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
@@ -110,12 +114,16 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = True 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
