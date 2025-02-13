7ff70ce7548ca3f448a1f48661aa620dae153c30
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2025-02-14 03:00:31 +0530

    doc pro

--- Assignment/Module4/docproject/docapp/urls.py
+++ Assignment/Module4/docproject/docapp/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('',views.index),
     path('profile/',views.profile,name='profile'),
     path('contact/',views.contact,name='contact'),
-]
+    path('signup/',views.signup,name='signup'),
+    path('userlogout/',views.userlogout),
+    path('create/', views.create_doctor, name='create_doctor'),
+    path('update/<int:pk>/', views.update_doctor, name='update_doctor'),
+    path('delete/<int:pk>/', views.delete_doctor, name='delete_doctor'),
+    path('book/<int:pk>/', views.book_appointment, name='book_appointment'),
+] 
